81e9373dac4979c598108173f3429e8a864ada45
Agus SM <mrschwartx@gmail.com>
2025-04-19 10:31:35 +0700

    feat(main): Gateway, Routing & Cross cutting concerns in Microservices

--- accounts/src/main/java/com/example/accounts/service/client/CardsFeignClient.java
+++ accounts/src/main/java/com/example/accounts/service/client/CardsFeignClient.java
@@ -2,7 +2,6 @@ package com.example.accounts.service.client;
 
 import com.example.accounts.dto.CardsDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- script-curl.sh
+++ script-curl.sh
@@ -6,8 +6,8 @@ curl -X GET http://localhost:8072/examplebank/cards/seeds/init
 curl -X GET http://localhost:8072/examplebank/loans/seeds/init
 
 # Fetch Customer Details
-curl -X GET "http://localhost:8072/examplebank/accounts/api/customers/fetchCustomerDetails?mobileNumber=0812345678" \
-     -H "Accept: application/json"
+     curl -X GET "http://localhost:8072/examplebank/accounts/api/customers/fetchCustomerDetails?mobileNumber=0812345678" \
+          -H "Accept: application/json"
 
 # Clean accounts, cards, loans
 curl -X GET http://localhost:8072/examplebank/accounts/seeds/clean
